e3d45599c93cd82f70a820e6d21187109b0a704a
John Jacob Muli <jacobmuli0729@gmail.com>
2026-07-05 02:05:42 +0800

    feat(map): route stage picking through map_select; retire grid stage_select [7.6]

--- scripts/character_select.gd
+++ scripts/character_select.gd
@@ -247,7 +247,7 @@ func _maybe_advance() -> void:
 	MatchSelection.p2_data = p2["data"]
 	MatchSelection.p2_color = p2["color"]
 
-	get_tree().change_scene_to_file("res://scenes/stage_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/map_select.tscn")
 
 
 func _on_back_pressed() -> void:
